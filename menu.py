0b48c6fa2357bf44bd02a75ef6f5ef7389b2bfc0
abraao <abraaofilipi12@gmail.com>
2025-02-14 19:39:24 -0300

    first commit

---
+++ menu.py
@@ -0,0 +1,99 @@
+from textUtils import *
+import time
+def desenhar_menu():
+    os.system('cls')
+    def progress_bar(done):
+        printBrightCyan("\rInicializando sistema: [{0:50s}] {1:.1f}%".format('#' * int(done * 50), done * 100), end='')
+            
+    def load():
+        for n in range(101):
+            progress_bar(n / 100)
+            time.sleep(0.02)
+            
+    load()
+    time.sleep(1)
+    os.system('cls')
+    
+    
+    # Título do menu
+    printTitleBar("⚽ MENU PRINCIPAL | DEVFIT 🥊", BRIGHT_CYAN, BOLD_BRIGHT_CYAN)
+    
+    # Caixa 1
+    drawRoundBorderBox(1, 4, 30, 3, color=BRIGHT_YELLOW)
+    gotoxy(3,5)
+    printColored("📃 | Consultar", color=BRIGHT_YELLOW)
+    
+    # Opções da caixa 1
+    drawRoundBorderBox(1, 7, 30, 8)
+    gotoxy(3,8)
+    print("1 - Alunos")
+    gotoxy(3,9)
+    print("2 - Funcionários")
+    gotoxy(3,10)
+    print("3 - Planos")
+    gotoxy(3,11)
+    print("4 - Equipamentos")
+    gotoxy(3,12)
+    print("5 - Fornecedores")
+
+    # Caixa 2
+    drawRoundBorderBox(31, 4, 30, 3, color=BRIGHT_GREEN)
+    gotoxy(33,5)
+    printColored("📂 | Cadastrar", color=BRIGHT_GREEN)
+    
+    # Opções da caixa 2
+    drawRoundBorderBox(31, 7, 30, 8)
+    gotoxy(33,8)
+    print("6 - Aluno")
+    gotoxy(33,9)
+    print("7 - Funcionário")
+    gotoxy(33,10)
+    print("8 - Plano")
+    gotoxy(33,11)
+    print("9 - Equipamento")
+    gotoxy(33,12)
+    print("10 - Fornecedor")
+
+    # Caixa 3
+    drawRoundBorderBox(61, 4, 30, 3, color=BRIGHT_BLUE)
+    gotoxy(63,5)
+    printColored("📝 | Atualizar", color=BRIGHT_BLUE)
+
+    # Opções da caixa 3
+    drawRoundBorderBox(61, 7, 30, 8)
+    gotoxy(63,8)
+    print("11 - Alunos")
+    gotoxy(63,9)
+    print("12 - Funcionário")
+    gotoxy(63,10)
+    print("13 - Plano")
+    gotoxy(63,11)
+    print("14 - Equipamento")
+    gotoxy(63,12)
+    print("15 - Fornecedor")
+    
+    # Caixa 4
+    drawRoundBorderBox(91, 4, 30, 3, color=RED)
+    gotoxy(93,5)
+    printColored("⛔ | Excluir", color=RED)
+    
+    # Opções da caixa 4
+    drawRoundBorderBox(91, 7, 30, 8)
+    gotoxy(93,8)
+    print("16 - Aluno")
+    gotoxy(93,9)
+    print("17 - Funcionário")
+    gotoxy(93,10)
+    print("18 - Plano")
+    gotoxy(93,11)
+    print("19 - Equipamento")
+    gotoxy(93,12)
+    print("20 - Fornecedor")
+
+    drawRoundBorderBox(1, 15, 32, 3, color=RED)
+    gotoxy(3,16)
+    printRed("Digite 0 para cancelar...")
+
+    # Input
+    drawRoundBorderBox(1, 18, 32, 3, color=BRIGHT_CYAN)
+    gotoxy(3,19)
---
+++ program.py
@@ -0,0 +1,133 @@
+import os
+from mysql_general_config import *
+import time
+from textUtils import *
+from menu import *
+
+# Conectar ao banco
+try:
+    conexao = conectar()
+    cursor = conexao.cursor()
+except Exception as e:
+    print("Erro ao conectar ao banco de dados: ", e)
+os.system("cls")
+
+
+# Funções de Listagem
+# Listar Alunos
+def listar_alunos():
+
+    resultado = executarConsulta("SELECT * FROM aluno")
+
+    print("="*35 + " Alunos " + "="*35)
+    print(f"{'Nome':30}{'CPF':30}{'Data de Nascimento':15}")
+    for linha in resultado:
+        print(f"{linha[1]:28} | {linha[2]:27} | {str(linha[3]):15}")
+    print('\n')
+    input("Pressione enter para continuar...")
+
+# Listar funcionários
+def listar_funcionarios():
+
+    resultado = executarConsulta("SELECT * FROM funcionario")
+
+    print("="*55 + " Funcionários " + "="*55)
+    print(f"{'ID':4}{'CPF':15}{'Nome':30}{'Cargo':18}{'Contato':18}{'Salário':18}{"Contratação":15}")
+    for linha in resultado:
+        print(f"{linha[0]:2} | {linha[1]:12} | {linha[2]:27} | {linha[4]:15} | {linha[6]:15} | {str(linha[7]):15} | {str(linha[8])}")
+    print('\n')
+    input("Pressione enter para continuar...")
+
+def listar_planos():
+    resultado = executarConsulta("SELECT * FROM plano")
+
+    print("="*35 + " Planos " + "="*35)
+    print(f"{'ID':4}{'Valor':18}{'Período':30}{"Descrição":15}")
+    for linha in resultado:
+        print(f"{linha[0]:2} | R$ {str(linha[1]):12} | {linha[2]:27} | {linha[3]:15}")
+    print('\n')
+    input("Pressione enter para continuar...")
+
+def listar_equipamentos():
+    return
+
+def listar_fornecedores():
+    return
+
+
+# Funções de Cadastro
+def cadastrar_funcionario():
+    return
+
+def cadastrar_aluno(): 
+    os.system("cls")
+    print("Cadastro de Alunos")
+    print("Digite os dados abaixo:\n")
+    nome = input("Nome: ")
+    cpf = input("CPF: ")
+    data_nasc = input("Data de Nascimento(aaaa-mm-dd): ")
+    endereco = input("Endereço: ")
+    contato = input("Contato: ")
+    turma = int(input("Turma: "))
+    plano = input("Plano(Mensal, trimestral ou semestral): ")
+
+    query = f"""INSERT INTO aluno(nome, cpf, nascimento, endereco, contato, turma, plano) VALUES('{nome}', '{cpf}', '{data_nasc}', '{endereco}', '{contato}', '{turma}', '{plano}')"""
+
+    
+    try:
+        executarComando(query)
+        print("Aluno cadastrado com sucesso!")
+        input("pressione enter para continuar...")
+    except Exception as e:
+        print("Erro ao cadastrar aluno: ", e)
+
+
+def cadastrar_equipamento():
+    return
+
+def cadastrar_fornecedor():
+    return
+
+
+def main():
+    while True:
+        os.system("cls")
+        desenhar_menu()
+        opcao = input("Digite a opção desejada: ")
+        try:
+            if opcao == "1":
+                os.system("cls")
+                listar_alunos()
+
+            elif opcao == "2":
+                os.system("cls")
+                listar_funcionarios()
+
+            elif opcao == "3":
+                os.system("cls")
+                listar_planos()
+
+            elif opcao == "6":
+                os.system("cls")
+                cadastrar_aluno()
+
+            elif opcao == "0":
+                os.system("cls")
+            def progress_bar(done):
+                printBrightYellow("\rEncerrando sistema: [{0:50s}] {1:.1f}%".format('#' * int(done * 50), done * 100), end='')
+            
+            def closing():
+                for n in range(101):
+                    progress_bar(n / 100)
+                    time.sleep(0.01)
+            
+            closing()
+            time.sleep(1)
+            os.system('cls')
+            printBrightCyan("Obrigado por utilizar nosso sistema!")
+            break
+        except Exception as e:
+            print("Opção inválida! Tente novamente...", e)
+            input("pressione enter para continuar...")
+
+main()
